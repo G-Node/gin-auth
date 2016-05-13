ed696b560c2da05df9da6365c23d318a7de951ff
M. Sonntag <michael.p.sonntag@gmail.com>
2016-05-13 20:24:04 +0200

    test: add testdata_removeExpired

---
+++ data/data_test.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+package data
+
+import "testing"
+
+func TestData_RemoveExpired(t *testing.T) {
+	InitTestDb(t)
+
+	numTokens := len(ListAccessTokens())
+	numGrantReqs := len(ListGrantRequests())
+	numSessions := len(ListSessions())
+
+	RemoveExpired()
+
+	if len(ListAccessTokens()) != numTokens-1 {
+		t.Errorf("Number of access tokens (%d) does not match expected number", numTokens)
+	}
+	if len(ListGrantRequests()) != numGrantReqs-1 {
+		t.Errorf("Number of grant requests (%d) does not match expected number", numGrantReqs)
+	}
+	if len(ListSessions()) != numSessions-1 {
+		t.Errorf("Number of sessions (%d) does not match expected number", numSessions)
+	}
+}
