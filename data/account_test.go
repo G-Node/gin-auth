f00f29c31f22c6597749abffb87ed1d12119dbc8
M. Sonntag <michael.p.sonntag@gmail.com>
2016-07-05 19:59:03 +0200

    test: add validate account test

--- data/account_test.go
+++ data/account_test.go
@@ -317,3 +317,95 @@ func TestAccount_Update(t *testing.T) {
 		t.Error("Account isDisabled was not updated")
 	}
 }
+
+func TestValidate(t *testing.T) {
+	InitTestDb(t)
+
+	const errDiffUser = "Please choose a different username"
+
+	account := &Account{}
+
+	// Test all data missing
+	valErr := account.Validate()
+	if valErr.Message == "" {
+		t.Error("Expected validation error")
+	}
+
+	account.FirstName = "fn"
+	account.LastName = "ln"
+	account.Login = "alice"
+	account.Email = "bob@foo.com"
+	account.Institute = "Inst"
+	account.Department = "Dep"
+	account.City = "cty"
+	account.Country = "ctry"
+
+	// Test existing login
+	valErr = account.Validate()
+	if valErr.FieldErrors["login"] != errDiffUser {
+		t.Errorf("Expected invalid username error, but got: '%s'", valErr.FieldErrors["login"])
+	}
+
+	// Test missing email
+	account.Login = "noone"
+	account.Email = ""
+	valErr = account.Validate()
+	if valErr.FieldErrors["email"] != "Please add email" {
+		t.Errorf("Expected missing email error, but got: '%s'", valErr.FieldErrors["email"])
+	}
+
+	// Test missing first name
+	account.Email = "noone@nowhere.com"
+	account.FirstName = ""
+	valErr = account.Validate()
+	if valErr.FieldErrors["first_name"] != "Please add first name" {
+		t.Errorf("Expected missing first name error, but got: '%s'", valErr.FieldErrors["first_name"])
+	}
+
+	// Test missing last name
+	account.FirstName = "fn"
+	account.LastName = ""
+	valErr = account.Validate()
+	if valErr.FieldErrors["last_name"] != "Please add last name" {
+		t.Errorf("Expected missing last name error, but got: '%s'", valErr.FieldErrors["last_name"])
+	}
+
+	// Test missing institute
+	account.LastName = "ln"
+	account.Institute = ""
+	valErr = account.Validate()
+	if valErr.FieldErrors["institute"] != "Please add institute" {
+		t.Errorf("Expected missing institute error, but got: '%s'", valErr.FieldErrors["institute"])
+	}
+
+	// Test missing department
+	account.Institute = "Inst"
+	account.Department = ""
+	valErr = account.Validate()
+	if valErr.FieldErrors["department"] != "Please add department" {
+		t.Errorf("Expected missing department error, but got: '%s'", valErr.FieldErrors["department"])
+	}
+
+	// Test missing city
+	account.Department = "Dep"
+	account.City = ""
+	valErr = account.Validate()
+	if valErr.FieldErrors["city"] != "Please add city" {
+		t.Errorf("Expected missing city error, but got: '%s'", valErr.FieldErrors["city"])
+	}
+
+	// Test missing country
+	account.City = "cty"
+	account.Country = ""
+	valErr = account.Validate()
+	if valErr.FieldErrors["country"] != "Please add country" {
+		t.Errorf("Expected missing country error, but got: '%s'", valErr.FieldErrors["country"])
+	}
+
+	// Test valid
+	account.Country = "ctry"
+	valErr = account.Validate()
+	if valErr.Message != "" {
+		t.Errorf("Expected valid registration , but got error in fields: '%s'", valErr.FieldErrors)
+	}
+}
