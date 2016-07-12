c21e2d980f453699427bdb04868c2936cf6fc7c2
M. Sonntag <michael.p.sonntag@gmail.com>
2016-07-12 19:09:31 +0200

    tests: add Reset tests

--- web/oauth_test.go
+++ web/oauth_test.go
@@ -1226,3 +1226,167 @@ func TestResetPage(t *testing.T) {
 		t.Errorf("Expected StatusOK on valid reset code but got '%d'", response.Code)
 	}
 }
+
+func TestReset(t *testing.T) {
+	handler := InitTestHttpHandler(t)
+	const resetURL = "/oauth/reset"
+	const codeKey = "ResetCode"
+	const codeInvalid = "iDoNotExist"
+	const codeDisabled = "rc_c"
+	const codeValid = "rc_a"
+	const codeValidInactive = "rc_b"
+
+	// Test empty body
+	request, _ := http.NewRequest("POST", resetURL, strings.NewReader(""))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response := httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusNotFound {
+		t.Errorf("Expected StatusNotFound on missing reset code but got '%d'", response.Code)
+	}
+
+	// Test invalid password reset code
+	mkBody := &url.Values{}
+	mkBody.Add(codeKey, codeInvalid)
+
+	request, _ = http.NewRequest("POST", resetURL, strings.NewReader(mkBody.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusNotFound {
+		t.Errorf("Expected StatusNotFound on invalid reset code but got '%d'", response.Code)
+	}
+
+	// Test valid password reset code of disabled account
+	mkBody.Set(codeKey, codeDisabled)
+
+	request, _ = http.NewRequest("POST", resetURL, strings.NewReader(mkBody.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusNotFound {
+		t.Errorf("Expected StatusNotFound on disabled reset code but got '%d'", response.Code)
+	}
+
+	// Test valid password reset code, missing password
+	mkBody.Set(codeKey, codeValid)
+	request, _ = http.NewRequest("POST", resetURL, strings.NewReader(mkBody.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Errorf("Expected StatusOK on valid reset code but got '%d'", response.Code)
+	}
+	if response.Header().Get("Warning") != "Please enter password and password control" {
+		t.Errorf("Expected empty password warning but got '%s'", response.Header().Get("Warning"))
+	}
+
+	// Test valid password reset code, password and password control missmatch
+	mkBody.Add("Password", "pw")
+	mkBody.Add("PasswordControl", "pwcontrol")
+	request, _ = http.NewRequest("POST", resetURL, strings.NewReader(mkBody.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Errorf("Expected StatusOK on valid reset code but got '%d'", response.Code)
+	}
+	if response.Header().Get("Warning") != "Provided password did not match password control" {
+		t.Errorf("Expected empty password warning but got '%s'", response.Header().Get("Warning"))
+	}
+
+	// Test valid password reset code, password too long
+	s := []string{}
+	for i := 0; i < 513; i++ {
+		s = append(s, "s")
+	}
+	js := strings.Join(s, "")
+
+	mkBody.Set("Password", js)
+	request, _ = http.NewRequest("POST", resetURL, strings.NewReader(mkBody.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Errorf("Expected StatusOK on valid reset code but got '%d'", response.Code)
+	}
+	if response.Header().Get("Warning") != "Entry too long, please shorten to 512 characters" {
+		t.Errorf("Expected empty password warning but got '%s'", response.Header().Get("Warning"))
+	}
+
+	// Test valid password reset code, reset of pw code
+	account, exists := data.GetAccountByResetPWCode(codeValid)
+	if !exists {
+		t.Errorf("Account with reset code '%s' does not exist", codeValid)
+	}
+	id := account.UUID
+	_, exists = data.GetAccount(id)
+	if exists {
+		t.Errorf("Account with id '%s' reset code '%s' should not be active", id, codeValid)
+	}
+	pwHash := account.PWHash
+
+	mkBody.Set("Password", "pw")
+	mkBody.Set("PasswordControl", "pw")
+	request, _ = http.NewRequest("POST", resetURL, strings.NewReader(mkBody.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Errorf("Expected StatusOK on valid reset code but got '%d'", response.Code)
+	}
+
+	account, exists = data.GetAccount(id)
+	if !exists {
+		t.Errorf("Account with id '%s' should be active", id)
+	}
+	if account.PWHash == pwHash {
+		t.Errorf("Password of Account with id '%s' has not been updated", id)
+	}
+	if account.ResetPWCode.String != "" {
+		t.Errorf("Password reset code of Account with id '%s' has not been deleted", id)
+	}
+
+	// Test valid password reset code, reset of pw code, reset of activation code
+	account, exists = data.GetAccountByResetPWCode(codeValidInactive)
+	if !exists {
+		t.Errorf("Account with reset code '%s' does not exist", codeValidInactive)
+	}
+	id = account.UUID
+	_, exists = data.GetAccount(id)
+	if exists {
+		t.Errorf("Account with id '%s' reset code '%s' should not be active", id, codeValidInactive)
+	}
+	pwHash = account.PWHash
+
+	mkBody.Set(codeKey, codeValidInactive)
+	request, _ = http.NewRequest("POST", resetURL, strings.NewReader(mkBody.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Errorf("Expected StatusOK on valid reset code but got '%d'", response.Code)
+	}
+
+	account, exists = data.GetAccount(id)
+	if !exists {
+		t.Errorf("Account with id '%s' should be active", id)
+	}
+	if account.PWHash == pwHash {
+		t.Errorf("Password of Account with id '%s' has not been updated", id)
+	}
+	if account.ResetPWCode.String != "" {
+		t.Errorf("Password reset code of Account with id '%s' has not been removed", id)
+	}
+	if account.ActivationCode.String != "" {
+		t.Errorf("Activation code of Account with id '%s' has not been removed", id)
+	}
+}
