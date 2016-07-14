33092dbe75818c242d6f453ba306b7116b1afc7a
M. Sonntag <michael.p.sonntag@gmail.com>
2016-07-14 13:51:25 +0200

    util: refactor send email

--- util/email.go
+++ util/email.go
@@ -15,6 +15,38 @@ import (
 	"text/template"
 )
 
+// EmailDispatcher defines an interface for e-mail dispatch.
+type EmailDispatcher interface {
+	Send(recipient []string, message []byte) error
+}
+
+// EmailConfig contains all information required for e-mail dispatch via smtp.
+type EmailConfig struct {
+	Identity   string
+	Dispatcher string
+	Password   string
+	Host       string
+	Port       string
+}
+
+type emailDispatcher struct {
+	conf EmailConfig
+	send func(string, smtp.Auth, string, []string, []byte) error
+}
+
+// Send sets up authentication for e-mail dispatch via smtp and invokes the objects send function.
+func (e *emailDispatcher) Send(recipient []string, content []byte) error {
+	addr := e.conf.Host + ":" + e.conf.Port
+	auth := smtp.PlainAuth(e.conf.Identity, e.conf.Dispatcher, e.conf.Password, e.conf.Host)
+	return e.send(addr, auth, e.conf.Dispatcher, recipient, content)
+}
+
+// NewSmtpSendMailDispatcher returns an instance of emailDispatcher
+// using smtp.SendMail as send function.
+func NewSmtpSendMailDispatcher(conf EmailConfig) EmailDispatcher {
+	return &emailDispatcher{conf, smtp.SendMail}
+}
+
 // MakePlainEmailTemplate returns a bytes.Buffer containing a standard e-mail
 func MakePlainEmailTemplate(from string, to []string, subj string, messageBody string) *bytes.Buffer {
 	const emailTemplate = `From: {{ .From }}
@@ -46,18 +78,4 @@ Subject: {{ .Subject }}
 		panic("Error executing e-mail template")
 	}
 	return &doc
-}
-
-// Send will send an e-mail with the handed message to the number of specified recipients.
-func Send(recipient []string, subject string, message []byte) error {
-
-	const identity = ""
-	const sender = ""
-	const pw = ""
-	const host = "mail.gmx.net"
-	const port = "587"
-
-	auth := smtp.PlainAuth(identity, sender, pw, host)
-
-	return smtp.SendMail(host+":"+port, auth, sender, recipient, message)
 }
