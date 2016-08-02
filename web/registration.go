482c6d36a8adf812e2ef55a6df13577d2e40fae9
M. Sonntag <michael.p.sonntag@gmail.com>
2016-08-02 11:31:38 +0200

    move registration functions

---
+++ web/registration.go
@@ -0,0 +1,240 @@
+// Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+package web
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/G-Node/gin-auth/conf"
+	"github.com/G-Node/gin-auth/data"
+	"github.com/G-Node/gin-auth/util"
+	"github.com/dchest/captcha"
+)
+
+type validateAccount struct {
+	*data.Account
+	*util.ValidationError
+	CaptchaId      string
+	CaptchaResolve string
+}
+
+// RegistrationPage displays entry fields required for the creation of a new gin account
+func RegistrationPage(w http.ResponseWriter, r *http.Request) {
+	valAccount := &validateAccount{}
+	valAccount.Account = &data.Account{}
+	valAccount.ValidationError = &util.ValidationError{}
+	valAccount.CaptchaId = captcha.New()
+
+	tmpl := conf.MakeTemplate("registration.html")
+	w.Header().Add("Cache-Control", "no-store")
+	w.Header().Add("Content-Type", "text/html")
+	err := tmpl.ExecuteTemplate(w, "layout", valAccount)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type passwordData struct {
+	Password        string
+	PasswordControl string
+}
+
+type registration struct {
+	verifyCaptcha func(string, string) bool
+}
+
+// RegistrationHandler provides an http handler for account registration.
+func RegistrationHandler(f func(string, string) bool) http.Handler {
+	rh := &registration{verifyCaptcha: f}
+	return rh
+}
+
+// The http handler of the registration class parses user entries for a new account. It will redirect back to the
+// entry form, if input is invalid. If the input is correct, it will create a new account,
+// send an e-mail with an activation link and redirect to the the registered page.
+func (rh *registration) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tmpl := conf.MakeTemplate("registration.html")
+	w.Header().Add("Cache-Control", "no-store")
+	w.Header().Add("Content-Type", "text/html")
+
+	account := &data.Account{}
+	pw := &passwordData{}
+
+	err := util.ReadFormIntoStruct(r, account, true)
+	if err != nil {
+		PrintErrorHTML(w, r, err, http.StatusInternalServerError)
+		return
+	}
+
+	err = util.ReadFormIntoStruct(r, pw, true)
+	if err != nil {
+		PrintErrorHTML(w, r, err, http.StatusInternalServerError)
+		return
+	}
+
+	valAccount := &validateAccount{}
+	valAccount.ValidationError = &util.ValidationError{}
+	valAccount.Account = account
+
+	if r.Form.Encode() == "" {
+		valAccount.Message = "Please add all required fields (*)"
+		err := tmpl.ExecuteTemplate(w, "layout", valAccount)
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	valAccount.ValidationError = valAccount.Account.Validate()
+
+	if pw.Password != pw.PasswordControl {
+		valAccount.FieldErrors["password"] = "Provided password did not match password control"
+		if valAccount.Message == "" {
+			valAccount.Message = valAccount.FieldErrors["password"]
+		}
+	}
+	if pw.Password == "" || pw.PasswordControl == "" {
+		valAccount.FieldErrors["password"] = "Please enter password and password control"
+		if valAccount.Message == "" {
+			valAccount.Message = valAccount.FieldErrors["password"]
+		}
+	}
+	if len(pw.Password) > 512 || len(pw.PasswordControl) > 512 {
+		valAccount.FieldErrors["password"] =
+			fmt.Sprintf("Entry too long, please shorten to %d characters", 512)
+		if valAccount.Message == "" {
+			valAccount.Message = valAccount.FieldErrors["password"]
+		}
+	}
+
+	captchaRes := r.PostForm.Get("captcha_resolve")
+	captchaId := r.PostForm.Get("captcha_id")
+	isCorrectCaptcha := rh.verifyCaptcha(captchaId, captchaRes)
+	if !isCorrectCaptcha {
+		valAccount.FieldErrors["password"] = "Please enter password and password control"
+		valAccount.FieldErrors["captcha"] = "Please resolve verification"
+		if valAccount.Message == "" {
+			valAccount.Message = valAccount.FieldErrors["captcha"]
+		}
+	}
+
+	if valAccount.Message != "" {
+		valAccount.CaptchaId = captcha.New()
+		err := tmpl.ExecuteTemplate(w, "layout", valAccount)
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	valAccount.Account.SetPassword(pw.Password)
+	valAccount.Account.ActivationCode = sql.NullString{String: util.RandomToken(), Valid: true}
+
+	err = account.Create()
+	if err != nil {
+		valAccount.Message = "An error occurred during registration."
+		err := tmpl.ExecuteTemplate(w, "layout", valAccount)
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	tmplFields := &struct {
+		From    string
+		To      string
+		Subject string
+		BaseUrl string
+		Code    string
+	}{}
+	tmplFields.From = conf.GetSmtpCredentials().From
+	tmplFields.To = account.Email
+	tmplFields.Subject = "GIN account activation"
+	tmplFields.BaseUrl = conf.GetServerConfig().BaseURL
+	tmplFields.Code = account.ActivationCode.String
+
+	content := util.MakeEmailTemplate("emailactivate.txt", tmplFields)
+	disp := util.NewEmailDispatcher()
+
+	err = disp.Send([]string{account.Email}, content.Bytes())
+	if err != nil {
+		msg := "An error occurred trying to send registration e-mail. Please contact an administrator."
+		PrintErrorHTML(w, r, msg, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Cache-Control", "no-store")
+	http.Redirect(w, r, "/oauth/registered_page", http.StatusFound)
+}
+
+// RegisteredPage displays information about how a newly created gin account can be activated.
+func RegisteredPage(w http.ResponseWriter, r *http.Request) {
+	head := "Account registered"
+	message := "Your account activation is pending. "
+	message += "An e-mail with an activation code has been sent to your e-mail address."
+
+	info := struct {
+		Header  string
+		Message string
+	}{head, message}
+
+	tmpl := conf.MakeTemplate("success.html")
+	w.Header().Add("Cache-Control", "no-store")
+	w.Header().Add("Content-Type", "text/html")
+	err := tmpl.ExecuteTemplate(w, "layout", info)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Activation removes an existing activation code from an account, thus rendering the account active.
+func Activation(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		PrintErrorHTML(w, r, "Activation request was malformed", http.StatusBadRequest)
+		return
+	}
+
+	getCode := r.Form.Get("activation_code")
+	if getCode == "" {
+		PrintErrorHTML(w, r, "Account activation code was absent", http.StatusBadRequest)
+		return
+	}
+
+	account, exists := data.GetAccountByActivationCode(getCode)
+	if !exists {
+		PrintErrorHTML(w, r, "Requested account does not exist", http.StatusNotFound)
+		return
+	}
+
+	account.ActivationCode.Valid = false
+	err = account.Update()
+	if err != nil {
+		panic(err)
+	}
+
+	head := "Account activation"
+	message := fmt.Sprintf("Congratulation %s %s! The account for %s has been activated and can now be used.",
+		account.FirstName, account.LastName, account.Login)
+	info := struct {
+		Header  string
+		Message string
+	}{head, message}
+
+	tmpl := conf.MakeTemplate("success.html")
+	w.Header().Add("Cache-Control", "no-store")
+	w.Header().Add("Content-Type", "text/html")
+
+	err = tmpl.ExecuteTemplate(w, "layout", info)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ web/registration_test.go
@@ -0,0 +1,217 @@
+// Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+package web
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/G-Node/gin-auth/conf"
+	"github.com/G-Node/gin-auth/data"
+)
+
+func TestRegistrationPage(t *testing.T) {
+	handler := InitTestHttpHandler(t)
+
+	request, _ := http.NewRequest("GET", "/oauth/registration_page", strings.NewReader(""))
+	response := httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+	if response.Code != http.StatusOK {
+		t.Errorf("Response code '%d' expected but was '%d'", http.StatusOK, response.Code)
+	}
+}
+
+func TestRegistrationHandler(t *testing.T) {
+	data.InitTestDb(t)
+	f := func(id string, resolve string) bool {
+		return id != "" && id == resolve
+	}
+	handler := RegistrationHandler(f)
+
+	const registrationURL = "/oauth/registration"
+	const registeredPageURL = "/oauth/registered_page"
+
+	body := &url.Values{}
+	body.Add("Title", "Title")
+	body.Add("FirstName", "First Name")
+	body.Add("MiddleName", "Middle Name")
+	body.Add("LastName", "Last Name")
+	body.Add("Login", "tl")
+	body.Add("Email", "testemail@example.com")
+	body.Add("IsEmailPublic", "true")
+	body.Add("Institute", "Institute")
+	body.Add("Department", "Department")
+	body.Add("City", "City")
+	body.Add("Country", "Country")
+	body.Add("IsAffiliationPublic", "true")
+	body.Add("Password", "pw")
+	body.Add("PasswordControl", "pw")
+
+	// test that a request without a posted form does not redirect to another page
+	request, _ := http.NewRequest("POST", registrationURL, strings.NewReader(""))
+	response := httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	redirect, err := url.Parse(response.Header().Get("Location"))
+	if err != nil {
+		t.Error(err)
+	}
+	if redirect.String() != "" {
+		t.Errorf("Expected empty location header, but was '%s'", redirect.String())
+	}
+
+	// test that a request with correct form content but missing captcha stays on the same page
+	request, _ = http.NewRequest("POST", registrationURL, strings.NewReader(body.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	redirect, err = url.Parse(response.Header().Get("Location"))
+	if err != nil {
+		t.Error(err)
+	}
+	if redirect.String() != "" {
+		t.Errorf("Expected empty location header, but was '%s'", redirect.String())
+	}
+
+	// test that a request with correct form content but incorrect captcha stays on the same page
+	body.Add("captcha_id", "test")
+	request, _ = http.NewRequest("POST", registrationURL, strings.NewReader(body.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	redirect, err = url.Parse(response.Header().Get("Location"))
+	if err != nil {
+		t.Error(err)
+	}
+	if redirect.String() != "" {
+		t.Errorf("Expected empty location header, but was '%s'", redirect.String())
+	}
+
+	// test that a request with correct form content redirects to registered_page
+	body.Add("captcha_resolve", "test")
+	request, _ = http.NewRequest("POST", registrationURL, strings.NewReader(body.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	redirect, err = url.Parse(response.Header().Get("Location"))
+	if err != nil {
+		t.Error(err)
+	}
+	if redirect.String() != registeredPageURL {
+		t.Errorf("Expected to be redirected to '%s', but was '%s'", registeredPageURL, redirect.String())
+	}
+
+	// Test error when sending e-mail
+	body.Set("Login", "tl2")
+	body.Set("Email", "testemail2@example.com")
+
+	mode := conf.GetSmtpCredentials().Mode
+	host := conf.GetSmtpCredentials().Host
+	conf.GetSmtpCredentials().Mode = ""
+	conf.GetSmtpCredentials().Host = "iDoNotExist.com"
+	request, _ = http.NewRequest("POST", registrationURL, strings.NewReader(body.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+
+	if response.Code != http.StatusInternalServerError {
+		t.Errorf("Expected StatusInternatServerError but got '%d'", response.Code)
+	}
+
+	// reset smtp configuration
+	conf.GetSmtpCredentials().Mode = mode
+	conf.GetSmtpCredentials().Host = host
+}
+
+func TestRegisteredPage(t *testing.T) {
+	handler := InitTestHttpHandler(t)
+
+	request, _ := http.NewRequest("GET", "/oauth/registered_page", strings.NewReader(""))
+	response := httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+	if response.Code != http.StatusOK {
+		t.Errorf("Response code '%d' expected but was '%d'", http.StatusOK, response.Code)
+	}
+}
+
+func TestActivation(t *testing.T) {
+	handler := InitTestHttpHandler(t)
+	const activationURL = "/oauth/activation"
+	const activationCodeDisabled = "ac_b"
+	const activationCode = "ac_a"
+
+	// Test missing query
+	request, _ := http.NewRequest("GET", activationURL, strings.NewReader(""))
+	response := httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+	if response.Code != http.StatusBadRequest {
+		t.Errorf("Expected StatusBadRequest on empty activationCode but got '%d'", response.Code)
+	}
+
+	// Test invalid activation code
+	request, _ = http.NewRequest("GET", activationURL, strings.NewReader(""))
+	q := request.URL.Query()
+	q.Add("activation_code", "iDoNotExist")
+	request.URL.RawQuery = q.Encode()
+
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+	if response.Code != http.StatusNotFound {
+		t.Errorf("Expected StatusNotFound on invalid activationCode but got '%d'", response.Code)
+	}
+
+	// Test activation code of disabled account
+	request, _ = http.NewRequest("GET", activationURL, strings.NewReader(""))
+	q = request.URL.Query()
+	q.Add("activation_code", activationCodeDisabled)
+	request.URL.RawQuery = q.Encode()
+
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+	if response.Code != http.StatusNotFound {
+		t.Errorf("Expected StatusNotFound on disabled account but got '%d'", response.Code)
+	}
+
+	// Test valid activation
+	account, exists := data.GetAccountByActivationCode(activationCode)
+	if !exists {
+		t.Errorf("Error on fetching account by activation code '%s'", activationCode)
+	}
+	if account.ActivationCode.String != activationCode {
+		t.Errorf("Expected activation code to be '%s' but got '%s'",
+			activationCode, account.ActivationCode.String)
+	}
+	accountLogin := account.Login
+
+	request, _ = http.NewRequest("GET", activationURL, strings.NewReader(""))
+	q = request.URL.Query()
+	q.Add("activation_code", activationCode)
+	request.URL.RawQuery = q.Encode()
+
+	response = httptest.NewRecorder()
+	handler.ServeHTTP(response, request)
+	if response.Code != http.StatusOK {
+		t.Errorf("Expected StatusOK on valid activationCode but got '%d'", response.Code)
+	}
+
+	account, exists = data.GetAccountByLogin(accountLogin)
+	if !exists {
+		t.Errorf("Error on fetching account by login '%s'", accountLogin)
+	}
+	if account.ActivationCode.String != "" || account.ActivationCode.Valid {
+		t.Errorf("Activation code should be cleared after activation but was '%s', '%t'",
+			account.ActivationCode.String, account.ActivationCode.Valid)
+	}
+}
