6970dc7f5087908c1a560b06906d2efbe7cf1b49
M. Sonntag <michael.p.sonntag@gmail.com>
2016-08-05 14:30:00 +0200

    test: add data email_create test

--- data/email_test.go
+++ data/email_test.go
@@ -9,7 +9,10 @@
 package data
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/G-Node/gin-auth/util"
 )
 
 func TestGetQueuedEmails(t *testing.T) {
@@ -21,4 +24,37 @@ func TestGetQueuedEmails(t *testing.T) {
 	if len(emails) < 1 {
 		t.Error("Expected queued e-mails, but result was empty")
 	}
+}
+
+func TestEmail_Create(t *testing.T) {
+	InitTestDb(t)
+
+	const recipient = "recipient@nowhere.com"
+	const content = "content"
+
+	emails, err := GetQueuedEmails()
+	if err != nil {
+		t.Errorf("Error fetching queued e-mails: '%s'\n", err.Error())
+	}
+	num := len(emails)
+
+	email := &Email{}
+	err = email.Create(util.NewStringSet(recipient), []byte(content))
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	emails, err = GetQueuedEmails()
+	if err != nil {
+		t.Errorf("Error fetching queued e-mails: '%s'\n", err.Error())
+	}
+	if len(emails) != num+1 {
+		t.Error("Queued e-mail was not properly created")
+	}
+
+	// TODO implement create for all modes
+
+	fmt.Printf("'%d', '%s', '%s', '%s', '%s', asByte: '%s', '%s'\n",
+		email.Id, email.Mode.String, email.Sender, email.Recipient.Strings()[0],
+		string(email.Content), email.Content, email.CreatedAt.String())
 }
