d902c31e301f0d31e27e053e18b4df82658d3b91
M. Sonntag <michael.p.sonntag@gmail.com>
2016-08-11 15:26:51 +0200

    conf: add init log

--- conf/log.go
+++ conf/log.go
@@ -9,6 +9,10 @@
 package conf
 
 import (
+	"io"
+	"os"
+
+	"github.com/NYTimes/logrotate"
 	"github.com/Sirupsen/logrus"
 )
 
@@ -21,3 +25,50 @@ type LogEnv struct {
 	Access *logrus.Logger
 	Close  func()
 }
+
+// InitLogEnv initializes loggers for access and error.
+// Default access log directs to Stdout, default error log
+// directs to Stderr. If log files are provided, the output
+// will be directed to the respective default and the log file.
+// Log files are opened using a logrotate compatible library.
+func InitLogEnv() {
+
+	accFile := "gin-auth.access.log"
+	errFile := "gin-auth.err.log"
+
+	logEnv = &LogEnv{
+		Access: logrus.New(),
+		Err:    logrus.New(),
+	}
+	logEnv.Access.Out = os.Stdout
+
+	fs := make([]*logrotate.File, 0, 2)
+
+	if accFile != "" {
+		af, err := logrotate.NewFile(accFile)
+		if err != nil {
+			panic(err)
+		}
+		logEnv.Access.Out = io.MultiWriter(os.Stdout, af)
+		fs = append(fs, af)
+	}
+
+	if errFile != "" {
+		ef, err := logrotate.NewFile(errFile)
+		if err != nil {
+			panic(err)
+		}
+		logEnv.Err.Out = io.MultiWriter(os.Stderr, ef)
+		fs = append(fs, ef)
+	}
+
+	logEnv.Close = func() {
+		for _, f := range fs {
+			f.Close()
+		}
+	}
+
+	logEnv.Access.Info("Access logging started")
+	logEnv.Err.Error("Error logging started")
+}
+
