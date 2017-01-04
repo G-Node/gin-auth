0161f406940e66556e9b8276e2534e636a98029d
M. Sonntag <michael.p.sonntag@gmail.com>
2017-01-04 16:30:26 +0100

    [conf/conf] add loading externals from server.yml

--- conf/conf.go
+++ conf/conf.go
@@ -366,3 +366,40 @@ func GetLogLocation() *LogLocations {
 
 	return logLoc
 }
+
+// Externals contains links to external resources e.g. required for links in templates.
+type Externals struct {
+	GinUiURL string
+}
+
+var externals *Externals
+var externalsLock = sync.Mutex{}
+
+// GetExternals loads the externals information from a yaml file when called the first time.
+func GetExternals() *Externals {
+	externalsLock.Lock()
+	defer externalsLock.Unlock()
+
+	if externals == nil {
+		content, err := ioutil.ReadFile(filepath.Join(configPath, serverConfigFile))
+		if err != nil {
+			panic(err)
+		}
+
+		e := &struct {
+			Externals struct {
+				GinUiURL string `yaml:"GinUiURL"`
+			}
+		}{}
+		err = yaml.Unmarshal(content, e)
+		if err != nil {
+			panic(err)
+		}
+
+		externals = &Externals{
+			GinUiURL: e.Externals.GinUiURL,
+		}
+	}
+
+	return externals
+}
